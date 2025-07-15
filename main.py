a6b10120b3b84aaa7138d956523401c81bccca61
Nick🛰️ <cpcodex23@gmail.com>
2025-07-15 02:36:50 -0400

    setup API url

--- main.py
+++ main.py
@@ -11,3 +11,23 @@ Display weather information in the terminal
 
 Handle all errors
 """
+# libraries
+import requests as req
+
+def sep():
+    print('=' * 60)
+    
+def req_url():
+    sep()
+    print('API URL:', r.url)
+    sep()
+
+# requests logic
+r = req.get('http://api.weatherapi.com/v1')
+r_type = '/current.json' # implement current weather data param
+payload = {'q': '26301', 'days': '2'} # parameters
+
+# format URL
+r = req.get(f'http://api.weatherapi.com/v1{r_type}', params=payload)
+# display URL
+req_url()
