bc374f5c6243601425693da0dc9172f2949dd1c5
Nick🛰️ <cpcodex23@gmail.com>
2025-07-15 04:27:19 -0400

    update. clean up logic

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ def req_url():
     print('API URL:', r.url)
     sep()
 
+# default URL
+link = 'http://api.weatherapi.com/v1'
+r = req.get(link)
+
 # requests logic
-r = req.get('http://api.weatherapi.com/v1')
-r_type = '/current.json' # implement current weather data param
-payload = {'q': '26301', 'days': '2'} # parameters
+cur_weath = '/current.json' # implement current weather data param
+forecast = '/forecast.json' # implement overall forecast
+
+# user init payload params
+# zip_code = int(input('Enter your Zip Code:\n'))
+# num_days = int(input('Enter number of Days: (1-7)\n'))
+zip_code = '26301'
+num_days = '2'
+payload = {'q': zip_code, 'days': num_days} # parameters
 
 # format URL
-r = req.get(f'http://api.weatherapi.com/v1{r_type}', params=payload)
+r = req.get(f'{link}{cur_weath}', params=payload)
 # display URL
 req_url()
+
+print("Status Code:", r.status_code)
