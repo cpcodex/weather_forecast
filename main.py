2b66b0bc07280022fe400928a82218357a49ed4d
Nick🛰️ <cpcodex23@gmail.com>
2025-07-14 21:16:14 -0400

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+API key from weather service provider
+
+utilize intall requests with pip
+
+make API request calls
+
+Parse the JSON data
+
+Display weather information in the terminal
+
+Handle all errors
+"""
