c1586617344dcb6ff87189448f1e4d5a15e0738e
Nick🛰️ <cpcodex23@gmail.com>
2025-07-16 01:37:52 -0400

    added in api logic and call request, status code: 200

--- main.py
+++ main.py
@@ -4,28 +4,57 @@ def req_url():
     def sep():
         print('=' * 65)
         
+    API_KEY = 'c04802b1cd664c84a8051503251507'
+    
+    # default URL
+    link = 'http://api.weatherapi.com/v1'
+    
+    # requests logic
+    cur_weath = '/current.json' # implement current weather data param
+    forecast = '/forecast.json' # implement overall forecast
+    
+    r = req.get(link)
+    
+    # user init payload params
+    zip_code = int(input('Enter your Zip Code:\n'))
+    num_days = int(input('Enter number of Days: (1-7)\n'))
+    # zip_code = '26301'
+    # num_days = '2'
+    
+    payload = {'key': API_KEY, 'q': zip_code, 'days': num_days} # parameters
+    
+    request_url = f'{link}{cur_weath}'
+    # format URL
+    r = req.get(request_url, params=payload) 
+    
+    # call information
+    info = r.json()
+    
+    # data table
+    location_data = info['location']
+    current_data = info['current']
+    
+    # format terminal
     sep()
     print('API URL:', r.url)
+    # status code for error handling
+    print("Status Code:", r.status_code)
     sep()
+    
+    # loop through key/value pairs
+    for key, value in current_data.items():
+        if key == 'condition':
+            print(f"condition: {value['text']}")
+        else:
+            print(f"{key}: {value}")
+    
+    sep()
+    # Location data loop     
+    for key, value in location_data.items():
+        if key == 'condition':
+            print(f"condition: {value['text']}")
+        else:
+            print(f"{key}: {value}")
 
-# default URL
-link = 'http://api.weatherapi.com/v1'
-r = req.get(link)
-
-# requests logic
-cur_weath = '/current.json' # implement current weather data param
-forecast = '/forecast.json' # implement overall forecast
-
-# user init payload params
-# zip_code = int(input('Enter your Zip Code:\n'))
-# num_days = int(input('Enter number of Days: (1-7)\n'))
-zip_code = '26301'
-num_days = '2'
-payload = {'q': zip_code, 'days': num_days} # parameters
-
-# format URL
-r = req.get(f'{link}{cur_weath}', params=payload)
 # display URL
 req_url()
-
-print("Status Code:", r.status_code)
